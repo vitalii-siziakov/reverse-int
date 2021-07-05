21474c487f0d10312ec6bb9e60bb31ac410c9c15
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2021-07-05 22:43:57 +0000

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+  let nS = String(Math.abs(n));
+  let nSresult = '';
+
+  for (let i = nS.length-1; i >= 0 ; i--) {
+    nSresult += nS[i];
+  };
+
+  return Number(nSresult);
+};
